70e4c78dde842cd57a965f1e997ae53e970689fa
1cefr09 <wangxrmail@163.com>
2023-11-03 16:07:24 +0800

    二维插值

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ y = [0,50,100]
 z = [[0.28,1.91,0.48],[1.14,5.49,1.44],[0.16,1.64,0.23]]
 
 # 创建二维插值函数
-interp_func = interpolate.interp2d(x, y, z, kind='cubic')
+interp_func = interpolate.interp2d(x, y, z, kind='linear')
 
 # 定义新的插值点坐标
 new_x = np.linspace(0, 100, 1000)
@@ -19,7 +19,9 @@ new_z = interp_func(new_x, new_y)
 # 绘制结果
 import matplotlib.pyplot as plt
 plt.imshow(new_z, cmap='jet', extent=[0, 100, 0, 100], origin='lower')
-plt.colorbar()
+
+cb=plt.colorbar()
+cb.set_label(label='KV/m',loc='top')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
