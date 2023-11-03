786e48cace0d4206395c1ae2da0dccaa48b18eac
1cefr09 <wangxrmail@163.com>
2023-11-03 11:32:54 +0800

    绘图制作完成，还需要插值

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-# 使用seaborn绘制热力图
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp2d
 
-# 假设你有以下四个点的x、y和z值
-x = [-10, -5, 5, 10]
-y = [-10, -5, 5, 10]
-z = [1, 2, 3, 4]
 
-# 将点转换为一个二维数组，其中每个元素代表对应位置的z值
-# 如果某个位置没有点，则用np.nan填充
-Z = np.full((21, 21), np.nan) # 创建一个21*21的空数组，因为x和y的范围是-10到10，共21个值
-for i in range(len(x)):
-    # 将x和y的值映射到数组的索引，注意要加上10，因为数组的索引从0开始
-    ix = x[i] + 10
-    iy = y[i] + 10
-    # 将z的值赋给对应位置的元素
-    Z[iy][ix] = z[i]
+if __name__ == '__main__':
+    # x = np.array(([[0, 10, 20, 30, 40, 50, 60, 70, 80]]))
+    # x_r = np.repeat(x, [8], axis=0)
+    # print(x_r.shape)
+    # print(x_r)
+    #
+    # y = np.array(([[80, 70, 60, 50, 40, 30, 20, 10]])).T
+    # y_r = np.repeat(y, [9], axis=1)
+    # print(y_r.shape)
+    # print(y_r)
 
-# 绘制热力图
-sns.heatmap(Z, center=0, vmin=0, vmax=4)
-# 添加标题
-plt.title('Heatmap of z values')
-# 显示图像
-plt.show()
+    x=np.arange(0, 1000,1)
+
+    y=np.arange(0, 1000,1)
+    z=np.zeros((1000,1000))
+    z[500,500]=1
+
+    c = plt.pcolormesh(x, y, z, cmap='viridis', shading='gouraud')# 彩虹热力图
+
+    plt.colorbar(c, label='AUPR')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
